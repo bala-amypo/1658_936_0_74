38821c1fb5f7095869636b8afde20c34d6ad65dd
bala-amypo <bala@amypo.in>
2025-12-17 09:29:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-
+import orgspringframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class StudentController(){
-@Autowired 
+@Autowired StudentService ser
 }
