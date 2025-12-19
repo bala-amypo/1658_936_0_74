952157997c18b2751c4752812d7f5f76ad028b49
bala-amypo <bala@amypo.in>
2025-12-19 08:01:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/TimestampController.java
+++ src/main/java/com/example/demo/controller/TimestampController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.demo.service.ValidationService;
 import com.example.demo.entity.ValidationEntity;
-import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
