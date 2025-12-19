795d9f70dba9fe15b5a5cf25e742f5ba3255bd9b
bala-amypo <bala@amypo.in>
2025-12-19 06:28:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -15,49 +15,5 @@ public class StudentEntity{
     private Date created;
 
 
-public void setCreated(Date created){
-    this.created=created;
-}
-public Date getCreated(){
-    return created;
-}
-
-public void setId(Integer id){
-    this.id=id;
-}
-public Integer getId(){
-    return id;
-}
-public void setName(String name){
-    this.name=name;
-}
-public String getName(){
-    return name;
-}
-
-
-public String getEmail(){
-    return email;
-}
-public void setEmail(String email){
-    this.email=email;
-
-}
-public String getPassword(){
-    return password;
-}
-public void setPassword(String password){
-    this.password=password;
-}
-public StudentEntity(Integer id,String name,String email,String password,Date created){
-    this.id=id;
-    this.name=name;
-    this.email=email;
-    this.password=password;
-    this.created=created;
-}
-public StudentEntity(){
-
-}
 
 }
--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -28,49 +28,4 @@ private Long id;
     private int age;
 
 
-public void setId(Long id){
-    this.id=id;
 }
-public Long getId(){
-    return id;
-}
-public void setName(String name){
-    this.name=name;
-}
-public String getName(){
-    return name;
-}
-
-
-public String getEmail(){
-    return email;
-}
-public void setEmail(String email){
-    this.email=email;
-
-}
-public String getPassword(){
-    return password;
-}
-public int getAge(){
-    return age;
-}
-public void setAge(int age){
-    this.age=age;
-}
-public ValidationEntity(Long id,
-@NotNull @Size(min=2,max=10,message ="must be 2 to 10 character") String name,
-    @Email(message="Email is not valid")String email,
-    @Size(min=3,max=6,message="must be 3 to 6 character") @NotNull(message="Password is mandatory")
-    @Max(30) @Positive(message="Age must be a positive number") int age){
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.password=password;
-        this.age=age;
-    }
-    public ValidationEntity(){
-
-    }
-}
-
