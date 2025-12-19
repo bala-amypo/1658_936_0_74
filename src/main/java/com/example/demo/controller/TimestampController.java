894278a12fbe99efd25082f7459f24ef5763cb3b
bala-amypo <bala@amypo.in>
2025-12-19 07:43:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/TimestampController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.demo.service.ValidationService;
+import com.example.demo.entity.ValidationEntity;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+public class ValidationController{
+@Autowired ValidationService ser;
+@PostMapping("/Timepost")
+
+public ValidationEntity sendData(@Valid @RequestBody ValidationEntity stu){
+    return ser.PostData(stu);
+}
+
+}
---
+++ src/main/java/com/example/demo/entity/Timestamp.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentEntity{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private Created LocalDateTime;
+    private Created LocalDateTime;
+
+
+
+}
